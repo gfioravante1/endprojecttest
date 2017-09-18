b466b22c84299586bda49dcce0331099997a0928
System <none>
2017-09-18 13:16:34 +0000

    🌊🎢 Updated with Glitch

---
+++ public/script.js
@@ -0,0 +1,15 @@
+/*
+  Project:   Hands-On Project 1-1
+	Author:    
+	Date:      
+	Purpose:   To demonstrate the basic structure of a web project.
+*/
+<script>
+     document.write("<ol>");
+     document.write("<li>Reduce spending on non-necessities.</li>");
+     document.write("<li>Use extra money to pay off debt, starting with the highest interest credit card.</li>");
+     document.write("<li>Continue paying off debts until you are debt free.</li>");
+     document.write("<li>Put a fixed percent of your pay aside in savings.</li>");
+     document.write("</ol>");
+</script>
+(<script src="script.js"></script>)
